9f6de35f28f6b870695cef41fd969fed2def9cc5
DebaPrasadbehura424 <debaprasadbehura89@gmail.com>
2026-08-12 05:29:31 +0530

    add

--- backend/controllers/PhoneController.cs
+++ backend/controllers/PhoneController.cs
@@ -1,6 +1,7 @@
 using backend.Data;
 using backend.models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 
 [ApiController]
@@ -14,18 +15,14 @@ public class PhoneController : ControllerBase
     {
         _db = db;
     }
-
-
     [HttpPost]
     public async Task<IActionResult> AddPhoneNumber(PhoneDetails phoneDetails)
     {
-
         try
         {
             _db.Phone.Add(phoneDetails);
             await _db.SaveChangesAsync();
             return Ok(phoneDetails);
-
         }
         catch (Exception ex)
         {
@@ -33,9 +30,57 @@ public class PhoneController : ControllerBase
             {
                 message = "There Some error during add" + ex.Message
             });
+        }
+    }
+
+
+    [HttpDelete("{id}/delete")]
+    public async Task<IActionResult> deletePhoneNumber(int id)
+    {
+        try
+        {
+
+            var phoneList = await _db.Phone.FindAsync(id);
+            if (phoneList == null)
+                return NotFound();
+
+            _db.Phone.Remove(phoneList);
+
+            await _db.SaveChangesAsync();
+
+            return Ok(new
+            {
+                message = "Number deleted successfully"
+            });
 
         }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new
+            {
+                message = "Error while deleting order",
+                error = ex.Message
+            });
+        }
+    }
 
+    [HttpGet]
+    public async Task<IActionResult> GetAllPhoneNumbers()
+    {
+        try
+        {
+            var phoneList = await _db.Phone.ToListAsync<PhoneDetails>();
+
+            return Ok(phoneList);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new
+            {
+                message = "Error while getting phone numbers",
+                error = ex.Message
+            });
+        }
     }
 
 }
--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13845b1cbea562486c259e9d88d64ab26ffc69b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b869b122cdc442b616050dc2846ed67ffc9f1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
